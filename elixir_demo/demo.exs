7c0fadd6f56cc7604393733f4521e12ebca8df9a
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-02-17 17:25:33 -0300

    Created simple working sphere ray marching

--- elixir_demo/demo.exs
+++ elixir_demo/demo.exs
@@ -1,148 +1,225 @@
 require OCLPolyHok
 
 # OCLPolyHok.set_debug_logs(true)
+# OCLPolyHok.TypeInference.set_debug_logs(true)
 
 defmodule SDL2 do
-  @on_load :load_nifs
-  def load_nifs do
-    :erlang.load_nif(~c"priv/demo_nifs", 0)
-  end
+	@on_load :load_nifs
+	def load_nifs do
+		:erlang.load_nif(~c"priv/demo_nifs", 0)
+	end
 
-  def loop() do
-    receive do
-      {:render_img, nx_array} ->
-        update_image_nx(nx_array)
+	def loop() do
+		receive do
+			{:render_img, nx_array} ->
+				update_image_nx(nx_array)
 
-      msg ->
-        IO.inspect(msg, label: "[SDL2] Received an unknown message")
-    end
+			msg ->
+				IO.inspect(msg, label: "[SDL2] Received an unknown message")
+		end
 
-    # Verifica se a janela do SDL2 recebeu um pedido de fechamento (como clicar no "X" da janela)
-    case close_requested_nif() do
-      true ->
-        IO.puts("[SDL2] Close requested. Exiting process.")
-        :ok
+		# Verifica se a janela do SDL2 recebeu um pedido de fechamento (como clicar no "X" da janela)
+		case close_requested_nif() do
+			true ->
+				IO.puts("[SDL2] Close requested. Exiting process.")
+				:ok
 
-      false ->
-        # Se não houve pedido de encerramento, chama a NIF para renderizar a janela e continua o loop
-        render_window_nif()
-        loop()
-    end
-  end
+			false ->
+				# Se não houve pedido de encerramento, chama a NIF para renderizar a janela e continua o loop
+				render_window_nif()
+				loop()
+		end
+	end
 
-  # Recebe um Nx armazenado na RAM e envia para a NIF de atualização da textura do SDL2
-  defp update_image_nx(%Nx.Tensor{data: %Nx.BinaryBackend{state: array}}) do
-    update_image_nif(array)
-  end
+	# Recebe um Nx armazenado na RAM e envia para a NIF de atualização da textura do SDL2
+	defp update_image_nx(%Nx.Tensor{data: %Nx.BinaryBackend{state: array}}) do
+		update_image_nif(array)
+	end
 
-  # --- NIF stubs ---
-  def create_window_nif(_title, _width, _height) do
-    :erlang.nif_error(:nif_not_implemented)
-  end
+	# --- NIF stubs ---
+	def create_window_nif(_title, _width, _height) do
+		:erlang.nif_error(:nif_not_implemented)
+	end
 
-  def close_requested_nif() do
-    :erlang.nif_error(:nif_not_implemented)
-  end
+	def close_requested_nif() do
+		:erlang.nif_error(:nif_not_implemented)
+	end
 
-  def render_window_nif() do
-    :erlang.nif_error(:nif_not_implemented)
-  end
+	def render_window_nif() do
+		:erlang.nif_error(:nif_not_implemented)
+	end
 
-  def update_image_nif(_img_array) do
-    :erlang.nif_error(:nif_not_implemented)
-  end
+	def update_image_nif(_img_array) do
+		:erlang.nif_error(:nif_not_implemented)
+	end
 end
 
 OCLPolyHok.defmodule RayMarching do
-  # Magnitude de um vetor 3D
-  defd lenght(v_x, v_y, v_z) do
-    return(sqrt(v_x * v_x + v_y * v_y + v_z * v_z))
-  end
+	# Magnitude de um vetor 3D
+	defd lenght(v_x, v_y, v_z) do
+		return(sqrt(v_x * v_x + v_y * v_y + v_z * v_z))
+	end
 
-  # Sphere Signed Distance Function
-  defd sphere_sdf(p_x, p_y, p_z, center_x, center_y, center_z, radius) do
-    # Calcula o vetor do ponto p para o centro da esfera
-    dx = p_x - center_x
-    dy = p_y - center_y
-    dz = p_z - center_z
+	# Cálculo de distancia euclidiana entre dois pontos 3D
+	defd distance(p1_x, p1_y, p1_z, p2_x, p2_y, p2_z) do
+		dx = p1_x - p2_x
+		dy = p1_y - p2_y
+		dz = p1_z - p2_z
+		return(sqrt(dx * dx + dy * dy + dz * dz))
+	end
 
-    # A distância do ponto p para a superfície da esfera é a magnitude do vetor menos o raio
-    return(lenght(dx, dy, dz) - radius)
-  end
+	# Sphere Signed Distance Function (SDF)
+	# A distância de um ponto p até a superfície de uma esfera é a distância do ponto p
+	# para o centro da esfera menos o raio da esfera.
+	# Os argumentos dessa função são um ponto p (p_x, p_y, p_z) e os parâmetros da esfera (center_x, center_y, center_z, radius).
+	defd sphere_sdf(p_x, p_y, p_z, center_x, center_y, center_z, radius) do
+		# Calcula a distância do ponto p para o centro da esfera usando a função de distância euclidiana
+		l = distance(p_x, p_y, p_z, center_x, center_y, center_z)
+		# Subtrai o raio da esfera
+		return(l - radius)
+	end
 
-  # 3D map of the scene, returns the distance to the closest object
-  defd scene_sdf(p_x, p_y, p_z) do
-    # For testing, I will just a single sphere in the center of the scene
-    s1_x = 0.0
-    s1_y = 0.0
-    s1_z = 0.0
-    s1_radius = 1.0
+	# 3D map of the scene, returns the distance to the closest object
+	# Receives a point p (p_x, p_y, p_z) and returns the distance to the closest object in the scene
+	# to that point
+	defd scene_sdf(p_x, p_y, p_z) do
+		# For testing, I will just add a single sphere in the center of the scene
+		# with radius 1
+		s1_x = 0.0
+		s1_y = 0.0
+		s1_z = -1.0
+		s1_radius = 0.5
 
-    return(sphere_sdf(p_x, p_y, p_z, s1_x, s1_y, s1_z, s1_radius))
-  end
+		return(sphere_sdf(p_x, p_y, p_z, s1_x, s1_y, s1_z, s1_radius))
+	end
 
-  defk render_kernel(img_array, dim_x, dim_y) do
-    tid_x = get_global_id(0)
-    tid_y = get_global_id(1)
+	# Ray marching function
+	# Receives a ray origin (ray_origin_x, ray_origin_y, ray_origin_z) and a ray direction (ray_dir_x, ray_dir_y, ray_dir_z)
+	# and returns the distance traveled along the ray until it hits an object or reaches a maximum distance
+	defd ray_march(ray_origin_x, ray_origin_y, ray_origin_z, ray_dir_x, ray_dir_y, ray_dir_z) do
+		total_distance = 0.0
 
-    p_x = 0.0
-    p_y = 1.0
-    p_z = -1.0
+		# Far clip plane
+		max_distance = 100.0
 
-    if tid_x < dim_x && tid_y < dim_y do
-      # foo = scene_sdf(p_x, p_y, p_z)
-      foo = -1.0
+		max_steps = 64
 
-      if foo < 0.0 do
-        # Se o ponto está dentro da esfera, pinta de vermelho
-        img_array[tid_y * dim_x + tid_x] = 0x0000FF
-      else
-        # Se o ponto está fora da esfera, pinta de preto
-        img_array[tid_y * dim_x + tid_x] = 0x000000
-      end
-    end
-  end
+		# Minimum distance to consider a hit
+		min_distance = 0.001
 
-  defp render_worker(sdl_pid, gpu_array, dim_x, dim_y) do
-    threads_per_block = 16
-    blocks_x = div(dim_x + threads_per_block - 1, threads_per_block)
-    blocks_y = div(dim_y + threads_per_block - 1, threads_per_block)
+		for i in range(0, max_steps) do
+			# Calculate the current point along the ray
+			current_x = ray_origin_x + ray_dir_x * total_distance
+			current_y = ray_origin_y + ray_dir_y * total_distance
+			current_z = ray_origin_z + ray_dir_z * total_distance
 
-    # Lançando o kernel de renderização na GPU
-    OCLPolyHok.spawn(
-      &RayMarching.render_kernel/3,
-      {blocks_x, blocks_y, 1},
-      {threads_per_block, threads_per_block, 1},
-      [gpu_array, dim_x, dim_y]
-    )
+			# Get the distance to the closest object from the current point
+			dist_to_closest = scene_sdf(current_x, current_y, current_z)
 
-    # Pega o resultado da renderização na GPU e envia para o processo do SDL2 para atualizar a textura
-    result_ram = gpu_array |> OCLPolyHok.get_gnx()
-    send(sdl_pid, {:render_img, result_ram})
+			if dist_to_closest < min_distance do
+				# Hit an object
+				return(total_distance)
+			end
 
-    # Recursão para continuar o loop de renderização
-    render_worker(sdl_pid, gpu_array, dim_x, dim_y)
-  end
+			if total_distance > max_distance do
+				# Exceeded maximum distance without hitting anything
+				# Indicate no hit with a negative value
+				return(-1.0)
+			end
 
-  def render(sdl_pid, gpu_array, dim_x, dim_y) do
-    render_worker_pid = spawn_link(fn -> render_worker(sdl_pid, gpu_array, dim_x, dim_y) end)
+			# Move along the ray by the distance to the closest object
+			total_distance = total_distance + dist_to_closest
+		end
 
-    sdl_monitor_ref = Process.monitor(sdl_pid)
+		# If we perform the maximum number of steps without hitting anything, we also consider it a miss
+		return(-1.0)
+	end
 
-    receive do
-      {:DOWN, ^sdl_monitor_ref, :process, _pid, _reason} ->
-        # Se o processo do SDL for encerrado, saímos do loop de renderização lindamente =)
-        IO.puts("[RayMarching] SDL process has terminated. Stopping render loop.")
+	defk render_kernel(img_array, dim_x, dim_y) do
+		tid_x = get_global_id(0)
+		tid_y = get_global_id(1)
 
-        Process.demonitor(sdl_monitor_ref, [:flush])
+		if tid_x < dim_x && tid_y < dim_y do
+			# Creating UV coordinates
+			u = 2.0 * ((1.0 * tid_x) / (1.0 * dim_x)) - 1.0
+			v = 2.0 * ((1.0 * tid_y) / (1.0 * dim_y)) - 1.0
+			# Aspect ratio correction
+			u = u * ((1.0 * dim_x) / (1.0 * dim_y))
 
-        # Unlink and kill render worker to ensure it doesn't keep running in the background
-        Process.unlink(render_worker_pid)
-        Process.exit(render_worker_pid, :kill)
+			# Set up camera origin (location)
+			cam_x = 0.0
+			cam_y = 0.0
+			cam_z = 0.0
 
-        :ok
-    end
-  end
+			# Ray direction (where the camera is looking at)
+			# The camera is looking towards the negative z direction
+			ray_dir_x = u
+			ray_dir_y = v
+			ray_dir_z = -1.0
+
+			# Normalize ray direction
+			ray_dir_len = lenght(ray_dir_x, ray_dir_y, ray_dir_z)
+			ray_dir_x = ray_dir_x / ray_dir_len
+			ray_dir_y = ray_dir_y / ray_dir_len
+			ray_dir_z = ray_dir_z / ray_dir_len
+
+			# Perform ray marching
+			dist = ray_march(cam_x, cam_y, cam_z, ray_dir_x, ray_dir_y, ray_dir_z)
+
+			if dist > 0.0 do
+				# We hit something, we will do a simple shading based on the distance traveled (closer objects are brighter)
+				brightness = 1.0 - (dist)
+				brightness = max(0.0, min(1.0, brightness)) # Clamp brightness to [0, 1]
+
+				img_array[tid_y * dim_x + tid_x] = trunc(brightness * 255.0)
+
+			else
+				# If we didn't hit anything, set the pixel to black
+				img_array[tid_y * dim_x + tid_x] = 0x000000
+			end
+		end
+	end
+
+	defp render_worker(sdl_pid, gpu_array, dim_x, dim_y) do
+		threads_per_block = 16
+		blocks_x = div(dim_x + threads_per_block - 1, threads_per_block)
+		blocks_y = div(dim_y + threads_per_block - 1, threads_per_block)
+
+		# Lançando o kernel de renderização na GPU
+		OCLPolyHok.spawn(
+			&RayMarching.render_kernel/3,
+			{blocks_x, blocks_y, 1},
+			{threads_per_block, threads_per_block, 1},
+			[gpu_array, dim_x, dim_y]
+		)
+
+		# Pega o resultado da renderização na GPU e envia para o processo do SDL2 para atualizar a textura
+		result_ram = gpu_array |> OCLPolyHok.get_gnx()
+		send(sdl_pid, {:render_img, result_ram})
+
+		# Recursão para continuar o loop de renderização
+		render_worker(sdl_pid, gpu_array, dim_x, dim_y)
+	end
+
+	def render(sdl_pid, gpu_array, dim_x, dim_y) do
+		render_worker_pid = spawn_link(fn -> render_worker(sdl_pid, gpu_array, dim_x, dim_y) end)
+
+		sdl_monitor_ref = Process.monitor(sdl_pid)
+
+		receive do
+			{:DOWN, ^sdl_monitor_ref, :process, _pid, _reason} ->
+				# Se o processo do SDL for encerrado, saímos do loop de renderização lindamente =)
+				IO.puts("[RayMarching] SDL process has terminated. Stopping render loop.")
+
+				Process.demonitor(sdl_monitor_ref, [:flush])
+
+				# Unlink and kill render worker to ensure it doesn't keep running in the background
+				Process.unlink(render_worker_pid)
+				Process.exit(render_worker_pid, :kill)
+
+				:ok
+		end
+	end
 end
 
 # Estrutura do programa:
